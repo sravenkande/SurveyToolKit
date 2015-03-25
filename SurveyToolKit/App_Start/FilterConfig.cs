3cdd9d6cab4c9f183d88f6b4951a39d2ecef92e0
Sraven <sravenkande@gmail.com>
2015-03-24 21:30:12 -0400

    Second Commit

--- SurveyToolKit/App_Start/FilterConfig.cs
+++ SurveyToolKit/App_Start/FilterConfig.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 namespace SurveyToolKit
 {
     // Test Commits for Git.
+    // Second Commit test for the Git.
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
